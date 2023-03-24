c4b66df0cf484c557c6689530377b49b1e61ab89
Nat Reyes <aylin.reyes.8@gmail.com>
2023-03-23 21:10:56 -0500

    Added Tdd for a button that allows you to change the color between red and blue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
+
+  // Log roles help me to identify the role of my elements.
+  // const { container } = render(<App />);
+  // logRoles(container);
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // we can use regular expressions in the name blue/i
+  // it is a good practice to give it a name
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  // it is recommended to type again what we are learning to get used to the syntax.
+  const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
+
+  // click element
+  fireEvent.click(colorBtn);
+  // bg color should change to blue
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'blue'});
+
+  //expect the btn text to be 'change to red'
+  expect(colorBtn).toHaveTextContent('Change to red');
 });
