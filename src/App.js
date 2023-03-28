567c891d77c63b194964c2d55e1dba0652867d00
Nat Reyes <aylin.reyes.8@gmail.com>
2023-03-27 20:54:15 -0500

    insert color change in app.js and refactor tests to work with it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
+
+export function replaceCamelWithSpaces(colorName) {
+    return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 function App() {
-    const [btnColor, setBtnColor] = useState('red');
+    const [btnColor, setBtnColor] = useState('MediumVioletRed');
     const [checkboxState, setCheckboxState] = useState(false);
-    const newBtnColor = btnColor === 'red' ? 'blue' : 'red';
+    const newBtnColor = btnColor === "MediumVioletRed" ? "MidnightBlue" : "MediumVioletRed";
 
     return (
         <div>
-            <button disabled={checkboxState} onClick={() => setBtnColor(newBtnColor)} style={{backgroundColor: btnColor}}> Change
-                to {newBtnColor}</button>
+            <button disabled={checkboxState} onClick={() => setBtnColor(newBtnColor)} style={{backgroundColor: checkboxState ? 'gray' : btnColor}}>  Change to {replaceCamelWithSpaces(newBtnColor)}</button>
             <input id="checkbox-btn" defaultChecked={checkboxState} type="checkbox" onChange={(e) => setCheckboxState(e.target.checked)} />
             <label htmlFor="checkbox-btn">Disable button</label>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { logRoles } from '@testing-library/dom';
+import { replaceCamelWithSpaces } from './App';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -12,29 +13,29 @@ test('button has correct initial color', () => {
 
   // we can use regular expressions in the name blue/i
   // it is a good practice to give it a name
-  const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
-  expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
+  const colorBtn = screen.getByRole('button', { name: 'Change to Midnight Blue'});
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
 });
 
 test('button turns blue when clicked', () => {
   render(<App />);
   // it is recommended to type again what we are learning to get used to the syntax.
-  const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
+  const colorBtn = screen.getByRole('button', { name: 'Change to Midnight Blue'});
 
   // click element
   fireEvent.click(colorBtn);
   // bg color should change to blue
-  expect(colorBtn).toHaveStyle({ backgroundColor: 'blue'});
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'MidnightBlue'});
 
   //expect the btn text to be 'change to red'
-  expect(colorBtn).toHaveTextContent('Change to red');
+  expect(colorBtn).toHaveTextContent('Change to Medium Violet Red');
 });
 
 test('Checkbox should exist and be uncheck, the button should be enabled', () => {
 
   render(<App/>);
   // Button should be enabled
-  const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
+  const colorBtn = screen.getByRole('button', { name: 'Change to Midnight Blue'});
   expect(colorBtn).toBeEnabled();
 
   // Checkbox is unchecked
@@ -47,7 +48,7 @@ test('When the checkbox is checked, button should be disabled', () => {
   render(<App/>);
   // initially checkbox should be unchecked
   // RTL is smart to know that an id that also belongs to a label is referencing the same component
-  const checkbox = screen.getByRole('checkbox', { name: "Disable bgutton"});
+  const checkbox = screen.getByRole('checkbox', { name: "Disable button"});
   const btnColor = screen.getByRole('button');
 
   expect(checkbox).not.toBeChecked();
@@ -58,3 +59,34 @@ test('When the checkbox is checked, button should be disabled', () => {
   expect(btnColor).toBeDisabled();
 })
 
+
+test('Button gets grey when is disabled', () => {
+
+  render(<App/>);
+  // initially checkbox should be unchecked
+  // RTL is smart to know that an id that also belongs to a label is referencing the same component
+  const checkbox = screen.getByRole('checkbox', { name: "Disable button"});
+  const btnColor = screen.getByRole('button');
+
+  // Click checkbox to disabled it
+  fireEvent.click(checkbox);
+  expect(btnColor).toHaveStyle({ backgroundColor: 'gray'});
+
+  // Click checkbox to enable it
+  fireEvent.click(checkbox);
+  expect(btnColor).toHaveStyle({ backgroundColor: 'MediumVioletRed'});
+})
+
+// with describe with define a scope of code more complex where we are going to test more than one functionality result
+// this is also the way a functionality which work with a function exported from other file
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
+});
